63b4a77ba3ceca4a5a7291e99f0682e7ae249007
Phil McDaniel <mcdanipc@gmail.com>
2020-01-19 12:45:16 -0600

    more work towards parsing chat logs. separated text. started to separate username

--- TwitchChatCrawler/TwitchChatCrawler.py
+++ TwitchChatCrawler/TwitchChatCrawler.py
@@ -1,12 +1,32 @@
 import requests
+import bs4 as soup
 from bs4 import BeautifulSoup
 
+#loop through site to get logs for all days in a month
+#loop through site to get logs for all months available
+
 #pick url to parse
-url = 'https://overrustlelogs.net/2mgovercsquared%20chatlog'
+url = 'https://overrustlelogs.net/2mgovercsquared%20chatlog/January%202020/2020-01-18'+ '.txt'
+#print (url)
+
 #get html from url via requests
 htmltext = requests.get(url).text
-#send html to bs4
-soup = BeautifulSoup(htmltext, 'html.parser')
-print(soup.prettify())
+# print(htmltext)
 
-#loop through all href= links. These are the months for the chatlogs
+#split text based on line ends
+lines = htmltext.splitlines()
+print(lines[1])
+
+#timestamp is 1-24, username = 25 through first colon after 25
+time = lines[1][1:24]
+print (time)
+
+username = lines[1][25:]
+print(username)
+
+#parse text from single day's chatlogs
+
+
+#separate datetime
+#separate user
+#serarate chatmessage
